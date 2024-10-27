6df9b6ebbeede226f0686e1a8302a70db34d2bdc
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-10-27 21:18:47 +0330

    Debugging TypeScript Applications

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,7 @@
 "use strict";
 let age = 24;
+if (age < 59) {
+    age += 10;
+}
+console.log(age);
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-// create a config file using this command:
-// tsc--init
+// for drugging ts file enable the "sourceMap": true in tsconfig.json and run tsc command to get index.js.map for debugging machine.
 
-// after modify the tsconfig.json, run this command to compile all .ts files:
-// tsc
+// in debug panel click on create a lunch.json file. this file tell to vs code how to debug our code.
+
+//in lunch.json add this config after program:
+// "preLaunchTask": "tsc: build - tsconfig.json",
+// with this setting, we tell vs code to use ts compiler to build application using the lunch.setting file
 
 let age: number = 24;
+if (age < 59) {
+  age += 10;
+}
+
+console.log(age);
