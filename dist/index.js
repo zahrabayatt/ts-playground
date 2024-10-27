7bccdac3cc341a70a165e3d241b1ba1eb62fac51
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-10-28 01:44:25 +0330

    Objects

--- dist/index.js
+++ dist/index.js
@@ -1,28 +1,15 @@
 "use strict";
-function calculateTax3(income) {
-    if (income < 50000) {
-        return income * 1.2;
-    }
-    return income * 1.3;
-}
-function calculateTax4(income) {
-    if (income < 50000) {
-        return income * 1.2;
-    }
-    return income * 1.3;
-}
-function example(optParam) {
-    if ((optParam || 9) < 10) {
-        console.log("hello");
-    }
-}
-example();
-function calculateTax5(income, taxYear = 2024) {
-    if (taxYear < 2022) {
-        return income * 1.2;
-    }
-    return income * 1.3;
-}
-calculateTax5(10000);
-calculateTax5(10000, 2022);
+let employee = { id: 1 };
+let user = {
+    id: 1,
+    name: "zahra",
+};
+let person = { id: 1, name: "Alex" };
+let customer = {
+    id: 1,
+    name: "Marry",
+    retire: (date) => {
+        console.log(date);
+    },
+};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,34 @@
-// ts annotate the return type of function for us but for best practice we should annotate it like parameters
+let employee = { id: 1 };
+// employee name = 'Mosh' // we got a compilation error
 
-// function calculateTax(income: number) {
-//   return 0;
-// }
+// we can annotate objects:
+let user: {
+  id: number;
+  name: string;
+  fax?: string; // we can make a property optical
+} = {
+  // when we defining a object we have to define all properties unless it is optional
+  id: 1,
+  name: "zahra",
+};
 
-// function calculateTax1(income: number): void {}
+// to make a property readonly we can use a readonly modifier:
+let person: {
+  readonly id: number;
+  name: string;
+} = { id: 1, name: "Alex" };
 
-// if we enable "noUnusedParameters": true, setting in tsconfig.json, then we got the warning for unused parameters in functions.
+// person.id = 12; // we got a compilation error because id is readonly
 
-//  function calculateTax2(income: number) {
-//  if (income < 50_000) {
-//     return income * 1.2;
-//   }
-
-// js by default return undefined for function, so the return type for this function is number | undefined but it might cause problems so we can enable this setting "noImplicitReturns": true, in tsconfig.json for getting warning when we don't specify return value explicitly or using annotate in function for all paths.
-// }
-
-function calculateTax3(income: number) {
-  if (income < 50_000) {
-    return income * 1.2;
-  }
-
-  return income * 1.3;
-}
-
-// we have another setting to detected unused local variables in tsconfig.ts:
-// "noUnusedLocals": true
-function calculateTax4(income: number): number {
-  // let x;
-
-  if (income < 50_000) {
-    return income * 1.2;
-  }
-
-  return income * 1.3;
-}
-
-// we can make a parameter optional:
-// or you can set a default value for parameter and make it optional:
-function example(optParam?: number): void {
-  if ((optParam || 9) < 10) {
-    // if optParm is undefined set the default value 9
-    console.log("hello");
-  }
-}
-
-example();
-
-// function calculateTax5(income: number, taxYear: number = 2024): number {
-// no need to annotate the optional parameter, it set explicity with value:
-function calculateTax5(income: number, taxYear = 2024): number {
-  if (taxYear < 2022) {
-    return income * 1.2;
-  }
-
-  return income * 1.3;
-}
-
-calculateTax5(10_000);
-calculateTax5(10_000, 2022);
+// how to define a method for object:
+let customer: {
+  id: number;
+  name: string;
+  retire: (date: Date) => void; // we need to specify and annotate the signature of method
+} = {
+  id: 1,
+  name: "Marry",
+  retire: (date: Date): void => {
+    console.log(date);
+  },
+};
