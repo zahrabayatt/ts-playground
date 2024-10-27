3ce9e8a5de8c815a2f5d69dd3bd104f4c74a924d
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-10-27 21:08:20 +0330

    Configuring the TypeScript Compiler

---
+++ src/index.js
@@ -0,0 +1,3 @@
+// create a config file using this command:
+// tsc--init
+var age = 24;
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+// create a config file using this command:
+// tsc--init
+
+// after modify the tsconfig.json, run this command to compile all .ts files:
+// tsc
+
+let age: number = 24;
