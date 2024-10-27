889e149692f9ffad063df3d81bf3ae2521efbba1
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-10-27 21:55:51 +0330

    Enums

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,21 @@
 "use strict";
-let user = [1, "Mosh"];
+const small = 1;
+const medium = 2;
+const large = 3;
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+var Answer;
+(function (Answer) {
+    Answer["Yes"] = "yes";
+    Answer["No"] = "no";
+    Answer["Maybe"] = "maybe";
+})(Answer || (Answer = {}));
+let mySize = Size.Large;
+console.log(mySize);
+let myGender = 1;
+console.log(myGender);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,36 @@
-let user: [number, string] = [1, "Mosh"];
+// Enum: represent a list of constants
 
-// tuple only useful when we have only two values!
+const small = 1;
+const medium = 2;
+const large = 3;
 
-let a: [number, string, boolean, number] = [1, "Mosh", true, 9];
+// you can use enums for these constants to group them:
 
-// because when tuple compile to js file it compile as array we have also function of array for tuple but don't use this one because it might make problems in your code:
-a.push("a");
+// for naming enum and its member use pascal naming convention.
+// by default ts compiler assign the members 0, 1, ..
+enum Size {
+  // if you set the members to number, you don't need to specify for each of them. you can only specify the first one and the rest of them explicity set.
+  Small = 1,
+  Medium, // ts explicity set the value to 1
+  Large, // ts explicity set the value to 2
+}
+
+enum Answer {
+  // if you set the values to string, you must specify for each of them.
+  Yes = "yes",
+  No = "no",
+  Maybe = "maybe",
+}
+
+// this is how we can use enum:
+let mySize: Size = Size.Large;
+console.log(mySize); // 3
+
+// if you define enum with const, the compiler generate more js optimize code:
+const enum Gender {
+  Male,
+  Female,
+}
+
+let myGender = Gender.Female;
+console.log(myGender);
