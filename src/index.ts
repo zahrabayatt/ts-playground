6c3d3233149c8e0b167b0bdaa8eadf5d496182ad
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-10-27 21:57:22 +0330

    How to run typescript file

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,5 @@
-// Enum: represent a list of constants
+// To run code first compile the ts file using tsc command then run the output file in dist folder using node:
+// tsc
+// node dist/index.js
 
-const small = 1;
-const medium = 2;
-const large = 3;
-
-// you can use enums for these constants to group them:
-
-// for naming enum and its member use pascal naming convention.
-// by default ts compiler assign the members 0, 1, ..
-enum Size {
-  // if you set the members to number, you don't need to specify for each of them. you can only specify the first one and the rest of them explicity set.
-  Small = 1,
-  Medium, // ts explicity set the value to 1
-  Large, // ts explicity set the value to 2
-}
-
-enum Answer {
-  // if you set the values to string, you must specify for each of them.
-  Yes = "yes",
-  No = "no",
-  Maybe = "maybe",
-}
-
-// this is how we can use enum:
-let mySize: Size = Size.Large;
-console.log(mySize); // 3
-
-// if you define enum with const, the compiler generate more js optimize code:
-const enum Gender {
-  Male,
-  Female,
-}
-
-let myGender = Gender.Female;
-console.log(myGender);
+console.log("Hello world!");
