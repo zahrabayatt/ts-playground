e0dddf551306192f6eea7f15a5571b8da7b2e20b
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-10-28 16:10:00 +0330

    The Nullish Coaelscing Operator

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,6 @@
 "use strict";
-var _a;
-function getCustomer(id) {
-    return id === 0 ? null : { birthday: new Date() };
-}
-let customer = getCustomer(0);
-if (customer !== null && customer !== undefined) {
-    console.log(customer.birthday);
-}
-console.log(customer === null || customer === void 0 ? void 0 : customer.birthday);
-console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
-let log = (message) => console.log(message);
-log = null;
-log === null || log === void 0 ? void 0 : log("a");
+let speed = null;
+let ride = {
+    speed: speed !== null && speed !== void 0 ? speed : 30,
+};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,10 @@
-type Customer = {
-  birthday?: Date;
+let speed: number | null = null;
+
+let ride = {
+  // Falsy values are undefined, null , '', false, 0, so the 0 value for speed is going to ignored
+  // speed: speed || 30,
+  // to fix that we can only check for nullability:
+  // speed: speed !== null ? speed : 30,
+  // in ts we have better way to checking null or undefined using nullish coaelscing operator (??):
+  speed: speed ?? 30, // if speed is not null or undefined use the speed's value otherwise use 30
 };
-
-function getCustomer(id: number): Customer | null | undefined {
-  return id === 0 ? null : { birthday: new Date() };
-}
-
-let customer = getCustomer(0);
-
-if (customer !== null && customer !== undefined) {
-  console.log(customer.birthday);
-}
-
-// There is a simpler way to writing this code in typescript:
-
-// we can use Optional property access operator (?) instead of checking customer is null or undefined:
-console.log(customer?.birthday);
-
-// if the birthday is a optional property:
-console.log(customer?.birthday?.getFullYear());
-
-// we have Optional element access operator:
-// customers?.[0]
-
-// we have Optional call operator:
-let log: any = (message: string) => console.log(message);
-log = null;
-// log could be null because the type of it is any:
-log?.("a");
