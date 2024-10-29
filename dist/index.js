14704c6fa926ab9032e90b3c38f979d5c72906a1
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-10-30 01:40:01 +0330

    Index Signatures and Properties

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,8 @@
 "use strict";
-class Account {
-    constructor(id, owner, _balance) {
-        this.id = id;
-        this.owner = owner;
-        this._balance = _balance;
-    }
-    deposit(amount) {
-        if (amount <= 0) {
-            throw new Error("Invalid amount");
-        }
-        this._balance += amount;
-    }
-    get balance() {
-        return this._balance;
-    }
+class SeatAssignment {
 }
-let account = new Account(1, "Zahra", 0);
-console.log(account.balance);
+let seats = new SeatAssignment();
+seats.A1 = "Zahra";
+seats.B1 = "Alex";
+seats["C2"] = "Marry";
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,10 @@
-class Account {
-  constructor(
-    public readonly id: number,
-    public owner: string,
-    private _balance: number
-  ) {}
-
-  deposit(amount: number): void {
-    if (amount <= 0) {
-      throw new Error("Invalid amount");
-    }
-    this._balance += amount;
-  }
-
-  //   getBalance(): number {
-  //     return this._balance;
-  //   }
-  // or we can use a getter:
-  get balance(): number {
-    return this._balance;
-  }
-
-  // we can also have a setter:
-
-  //   set balance(value: number) {
-  //     if (value < 0) {
-  //       throw new Error("Invalid value");
-  //     }
-  //     this._balance = value;
-  //   }
+// using indexed property we can create properties dynamically like JS but we got type checking
+class SeatAssignment {
+  // indexed property
+  [seatNumber: string]: string;
 }
 
-let account = new Account(1, "Zahra", 0);
-
-console.log(account.balance);
-// account.balance = 1; // we got the error because it it private
+let seats = new SeatAssignment();
+seats.A1 = "Zahra";
+seats.B1 = "Alex";
+seats["C2"] = "Marry";
