c3df32fe6e5b2d7df553a4503f22349c2738091e
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-11-06 00:20:08 +0330

    Extending Generic Classes

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,26 @@
 "use strict";
-var _a;
-function fetch(url) {
-    console.log(url);
-    return {
-        data: null,
-        error: null,
-    };
+class Store {
+    constructor() {
+        this._objects = [];
+    }
+    add(obj) {
+        this._objects.push(obj);
+    }
 }
-let result = fetch("url");
-console.log((_a = result.data) === null || _a === void 0 ? void 0 : _a.username);
+class CompressibleStore extends Store {
+    compress() { }
+}
+let store = new CompressibleStore();
+store.compress();
+class SearchableStore extends Store {
+    find(name) {
+        return this._objects.find((obj) => obj.name === name);
+    }
+}
+let myStore = new SearchableStore();
+class ProductStore extends Store {
+    filterByCategory(category) {
+        return this._objects.filter((obj) => obj.category === category);
+    }
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,44 @@
-// sometimes we need to constraint or limit the generic type arguments: limit the type of argument that we can pass as generic argument like T
-function echo<T extends number | string>(value: T): T {
-  return value;
-}
-
-echo("1");
-echo(1);
-
-// or we can limit the generic arguments base on the shape of the object
-function echo1<T extends { name: string }>(value: T): T {
-  return value;
-}
-
-echo1({ name: "a" });
-
-// or we can constraint by interface
-interface Person {
+interface Product {
   name: string;
+  price: number;
+  category: string;
 }
 
-function echo2<T extends Person>(value: T): T {
-  return value;
+class Store<T> {
+  // you must initialize properties that you defined in class directly with a value or in the constrictor when you carate a instance of class.
+  protected _objects: T[] = [];
+
+  add(obj: T): void {
+    this._objects.push(obj);
+  }
 }
 
-echo2({ name: "a" });
+// when we have extending the generic class we have three options:
 
-// or we can constraint by class
-
-class User {
-  constructor(public name: string) {}
+// Extending Option 1
+// Pass on the generic type parameter in inheritance from generic parent class to child class
+class CompressibleStore<T> extends Store<T> {
+  compress() {}
 }
 
-class Customer extends User {}
+let store = new CompressibleStore<Product>();
+store.compress();
 
-function echo3<T extends User>(value: T): T {
-  return value;
+// Extending Option 2
+// Restrict the generic type parameter
+// as long as object has a property named name with type string we can use it as T argument, the rest of properties doesn't meters for example product has name: string and also another properties (we constraints the shape of the object that we can use as generic arguments):
+class SearchableStore<T extends { name: string }> extends Store<T> {
+  find(name: string): T | undefined {
+    return this._objects.find((obj) => obj.name === name);
+  }
 }
 
-echo3({ name: "a" });
+let myStore = new SearchableStore<Product>();
+
+// Extending Option 3
+// Fix the generic type parameter
+class ProductStore extends Store<Product> {
+  filterByCategory(category: string): Product[] {
+    return this._objects.filter((obj) => obj.category === category);
+  }
+}
