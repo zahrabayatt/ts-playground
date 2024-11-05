905a29a4ed2ecf6db4922473d869e7b00c86a564
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-11-06 00:01:32 +0330

    Generic Constraints

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,39 @@
-interface Result<T> {
-  data: T | null;
-  error: string | null;
+// sometimes we need to constraint or limit the generic type arguments: limit the type of argument that we can pass as generic argument like T
+function echo<T extends number | string>(value: T): T {
+  return value;
 }
 
-function fetch<T>(url: string): Result<T> {
-  console.log(url);
-  return {
-    data: null,
-    error: null,
-  };
+echo("1");
+echo(1);
+
+// or we can limit the generic arguments base on the shape of the object
+function echo1<T extends { name: string }>(value: T): T {
+  return value;
 }
 
-interface User {
-  username: string;
+echo1({ name: "a" });
+
+// or we can constraint by interface
+interface Person {
+  name: string;
 }
 
-interface Product {
-  title: string;
+function echo2<T extends Person>(value: T): T {
+  return value;
 }
 
-let result = fetch<User>("url");
-console.log(result.data?.username);
+echo2({ name: "a" });
+
+// or we can constraint by class
+
+class User {
+  constructor(public name: string) {}
+}
+
+class Customer extends User {}
+
+function echo3<T extends User>(value: T): T {
+  return value;
+}
+
+echo3({ name: "a" });
