b23b75f9e0c25ddd3a92a729dc78fee876238bf0
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-11-06 02:08:23 +0330

    Type Mapping

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,50 @@
+// sometimes we need base a type of another type, this is called type mapping:
+
 interface Product {
   name: string;
   price: number;
 }
 
-class Store<T> {
-  protected _objects: T[] = [];
+// we using type mapping to create a type that is like Product but all properties is readonly:
 
-  add(obj: T): void {
-    this._objects.push(obj);
-  }
+// we use type alias because you can do type mapping in interfaces:
+type ReadOnlyProduct = {
+  // to create properties we use:
+  // Index signature
+  // keyof operator: using it, we got all keys or property of Product
+  // in operator: using it, we iterate over keyof Product and in each iteration Product is equal to one of the key(property name) of product
+  // Product[Property] is type of property
+  readonly [Property in keyof Product]: Product[Property];
+};
 
-  // T is Product
-  // keyof T is actual properties of Product which is a union type of product properties => "name" | "price"
-  // we don't define property argument as string because when we use obj[Property] compiler thinks that we use index signatures (Check the index signatures and properties commit)
-  find(property: keyof T, value: unknown): T | undefined {
-    return this._objects.find((obj) => obj[property] === value);
-  }
-}
+let product: ReadOnlyProduct = {
+  name: "a",
+  price: 1,
+};
+// product.name = 'b' // error - it is readonly property
 
-let store = new Store<Product>();
-store.add({ name: "a", price: 1 });
-store.find("name", "a");
-store.find("price", 1);
-// store.find("noneExistingValue", 1); // the product doesn't have any property that named noneExistingValue
+// or we can create a generic readonly type:
+type ReadOnly<T> = {
+  readonly [K in keyof T]: T[K];
+};
+
+let anotherProduct: Readonly<Product> = {
+  name: "a",
+  price: 1,
+};
+// anotherProduct.name = 'b' // error - it is readonly property
+
+// Example
+// using type mapping we can create a type and make each property optical:
+type Optional<T> = {
+  [K in keyof T]?: T[K];
+};
+
+// Example
+// using type mapping we can create a type and make each property nullable:
+type Nullable<T> = {
+  [K in keyof T]: T[K] | null;
+};
+
+// because these types are pretty useful they are actually built-in in typescript:
+// typescript utility types docs: https://www.typescriptlang.org/docs/handbook/utility-types.html
