3f04c96af56d7d47d9c7fa9c736e32edd64aa0e7
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-11-06 00:34:53 +0330

    The keyof Operator

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,25 @@
 interface Product {
   name: string;
   price: number;
-  category: string;
 }
 
 class Store<T> {
-  // you must initialize properties that you defined in class directly with a value or in the constrictor when you carate a instance of class.
   protected _objects: T[] = [];
 
   add(obj: T): void {
     this._objects.push(obj);
   }
-}
 
-// when we have extending the generic class we have three options:
-
-// Extending Option 1
-// Pass on the generic type parameter in inheritance from generic parent class to child class
-class CompressibleStore<T> extends Store<T> {
-  compress() {}
-}
-
-let store = new CompressibleStore<Product>();
-store.compress();
-
-// Extending Option 2
-// Restrict the generic type parameter
-// as long as object has a property named name with type string we can use it as T argument, the rest of properties doesn't meters for example product has name: string and also another properties (we constraints the shape of the object that we can use as generic arguments):
-class SearchableStore<T extends { name: string }> extends Store<T> {
-  find(name: string): T | undefined {
-    return this._objects.find((obj) => obj.name === name);
+  // T is Product
+  // keyof T is actual properties of Product which is a union type of product properties => "name" | "price"
+  // we don't define property argument as string because when we use obj[Property] compiler thinks that we use index signatures (Check the index signatures and properties commit)
+  find(property: keyof T, value: unknown): T | undefined {
+    return this._objects.find((obj) => obj[property] === value);
   }
 }
 
-let myStore = new SearchableStore<Product>();
-
-// Extending Option 3
-// Fix the generic type parameter
-class ProductStore extends Store<Product> {
-  filterByCategory(category: string): Product[] {
-    return this._objects.filter((obj) => obj.category === category);
-  }
-}
+let store = new Store<Product>();
+store.add({ name: "a", price: 1 });
+store.find("name", "a");
+store.find("price", 1);
+// store.find("noneExistingValue", 1); // the product doesn't have any property that named noneExistingValue
