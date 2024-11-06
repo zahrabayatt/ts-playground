1480702effd07c925c51880a638c8ebe762cc35e
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-11-06 19:44:36 +0330

    Property Decorators

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,36 @@
-// how to create a decorator to applied to accessors meaning getters and setters:
+// how to create a decorator to enhancing properties:
 
-// parameters of accessor decorators are like method decorators:
-function Capitalize(
-  target: any,
-  methodName: string,
-  descriptor: PropertyDescriptor
-) {
-  console.log(target, methodName);
-  const original = descriptor.get;
-  // getters doesn't have any arguments
-  descriptor.get = function () {
-    // original(getter) can be undefined
-    const result = original?.call(this);
+function MinLength(length: number) {
+  return (target: any, propertyName: string) => {
+    let value: string;
 
-    return typeof result === "string" ? result.toUpperCase() : result;
+    const descriptor: PropertyDescriptor = {
+      get() {
+        return value;
+      },
+      set(newValue: string) {
+        if (newValue.length < length) {
+          throw new Error(
+            `${propertyName} should be at least ${length} characters.`
+          );
+        }
+        value = newValue;
+      },
+    };
+
+    Object.defineProperty(target, propertyName, descriptor);
   };
 }
 
-class Person {
-  constructor(public firstName: string, public lastName: string) {}
+class User {
+  @MinLength(4)
+  password: string;
 
-  @Capitalize
-  get fullName() {
-    //return `${this.firstName} ${this.lastName}`;
-    //return null;
-    return 0;
+  constructor(password: string) {
+    this.password = password;
   }
 }
 
-let person = new Person("Alex", "King");
-console.log(person.fullName);
+let user = new User("1234");
+// user.password = "1";
+console.log(user.password);
