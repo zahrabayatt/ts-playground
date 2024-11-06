cc6870921e6d05e2e19099015c9d6755a60eaa11
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-11-06 16:20:00 +0330

    Parameterized Decorators

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,20 @@
-// how to create a decorator that we can apply to our class:
+// sometimes we need to pass arguments to our decorators, this is how we create parameterize decorators:
 
-// we use pascal naming for decorator names
-// parameter of decorator that we applied to class is constructor of class
-function Component(constructor: Function) {
-  console.log("Component decorator called");
-  // every object in JS has a prototype from which it inherits various properties and methods, so each class or it's members that has this decorator tha have these new members:
-  constructor.prototype.uniqueId = Date.now();
-  constructor.prototype.insertInDOM = () => {
-    console.log("Inserting the component in the DOM");
+type ComponentOptions = {
+  selector: string;
+};
+
+// Decorator factory
+function Component(options: ComponentOptions) {
+  return (constructor: Function) => {
+    console.log("Component decorator called");
+    constructor.prototype.options = options;
+    constructor.prototype.uniqueId = Date.now();
+    constructor.prototype.insertInDOM = () => {
+      console.log("Inserting the component in the DOM");
+    };
   };
 }
 
-@Component
+@Component({ selector: "#my-profile" })
 class ProfileComponent {}
-
-// we can do this using inheritance:
-
-class Component1 {
-  uniqueId: number = Date.now();
-  insertInDOM(): void {
-    console.log("Inserting the component in the DOM");
-  }
-}
-
-class ProfileComponent1 extends Component1 {}
