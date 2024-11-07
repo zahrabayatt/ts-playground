780943e4d1eb650913ddafc4a7ac1a8cc2ff4365
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-11-07 18:55:44 +0330

    Using Definitely Typed Declaration Files

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import { calculateTax } from "./tax";
+import * as _ from "lodash"; // we got the error because it is a pure js package - Could not find a declaration file for module 'lodash'. 'c:/Users/LENOVO/source/repos/ts-playground/node_modules/lodash/lodash.js' implicitly has an 'any' type.
+//   Try `npm i --save-dev @types/lodash` if it exists or add a new declaration (.d.ts) file containing `declare module 'lodash';`
 
-// const tax = calculateTax("hello"); // we got runtime error the income is number
-const tax = calculateTax(10_000);
-console.log(tax);
+// to solve this as in the error message says we using a popular github repository:
+// https://github.com/DefinitelyTyped/DefinitelyTyped
+// and we need to install types for lodash as dev dependencies:
+// npm i -D @types/lodash (npm i -D (or --save-dev) @types/target-package-name)
 
-// when using declaration files we should declare all the functions from .js files otherwise we can import it somewhere else
+_.clone([1, 2, 3]);
+
+// but newer repository comes with its types(declaration files) and we don't need to separately install its types from DefinitelyTyped like chalk
