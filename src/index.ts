cfabdfd3f849a203b552b2a2ba6bcbeba7e3bad6
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-11-07 18:20:52 +0330

    Including JS Code in TS Projects

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+// to import js code, enable this setting in tsconfig.json:
+// "allowJs": true,                                  /* Allow JavaScript files to be a part of your program. Use the 'checkJS' option to get errors from these files. */
 
+import { calculateTax } from "./tax";
+
+const tax = calculateTax(10000);
+console.log(tax);
